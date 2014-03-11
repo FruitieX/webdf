4eb8869722f38486c0c3b7c946cce57ed70500fd
Rasmus Eskola <fruitiex@gmail.com>
2014-03-11 17:08:13 +0200

    initial socket.io serverside code

--- webserver.js
+++ webserver.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
 // serve a nice webpage on port 8000
-var PORT = 8080;
+var WWW_PORT = 8080;
+var IO_PORT = 8081
 var static = require('node-static');
+var io = require('socket.io').listen(IO_PORT);
 
 var file = new static.Server('./');
 
@@ -10,10 +12,17 @@ require('http').createServer(function(req, res) {
 	req.addListener('end', function () {
 		file.serve(req, res);
 	}).resume();
-}).listen(PORT);
-console.log('listening on port ' + PORT);
+}).listen(WWW_PORT);
+console.log('web server listening on port ' + WWW_PORT);
 
 process.on('uncaughtException', function (err) {
 	console.error(err.stack);
 	console.log("ERROR! Node not exiting.");
 });
+
+io.sockets.on('connection', function(socket) {
+	socket.on('pos', function(data) {
+		console.log('pos update: ' + data);
+	});
+});
+console.log('socket.io listening on port ' + IO_PORT);
