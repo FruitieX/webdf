d06a29dae9df34a92e350edd67e12740cda826a5
Rasmus Eskola <fruitiex@gmail.com>
2014-03-15 22:47:39 +0200

    respawn and lower score if we fall out of map

--- game.js
+++ game.js
@@ -207,8 +207,7 @@ var shoot = function (){
 						'uid': uid
 					});
 
-					score++;
-					$("#score").html("Score: " + score);
+					updateScore(1);
 					return;
 				}
 			});
@@ -342,6 +341,23 @@ function doMove(delta) {
 	}
 }
 
+var updateScore = function(cnt) {
+	score += cnt;
+	$("#score").html("Score: " + score);
+}
+
+var respawn = function(reason) {
+	console.log(reason);
+	var yawObject = controls.getObject();
+	// neat hardcoded spawnpoint for now :)
+	yawObject.position.x = 0;
+	yawObject.position.y = 10;
+	yawObject.position.z = 0;
+	velocity.x = 0;
+	velocity.y = 0;
+	velocity.z = 0;
+}
+
 function animate() {
 
 	requestAnimationFrame( animate );
@@ -356,6 +372,12 @@ function animate() {
 
 	renderer.render( scene, camera );
 	time = Date.now();
+
+	// we're falling and fast... probably fell out of map!
+	if(velocity.y < -10) {
+		updateScore(-1);
+		respawn("Fell out of map");
+	}
 }
 
 socket.on('update', function(data) {
@@ -391,12 +413,7 @@ socket.on('update', function(data) {
 });
 
 socket.on('hit', function(data) {
-	console.log("you got hit!");
-	var yawObject = controls.getObject();
-	// neat hardcoded spawnpoint for now :)
-	yawObject.position.x = 0;
-	yawObject.position.y = 10;
-	yawObject.position.z = 0;
+	respawn("You got hit!");
 });
 
 socket.on('p_disconnected', function(data) {
