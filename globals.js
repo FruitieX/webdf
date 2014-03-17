aa132d0c7b6fb00b66de0a254dc44118fbfba8af
Rasmus Eskola <fruitiex@gmail.com>
2014-03-18 00:06:50 +0200

    giant restructure / cleanup

---
+++ globals.js
@@ -0,0 +1,37 @@
+// rendering
+var camera, scene, renderer;
+var geometry, material, mesh;
+var time = Date.now();
+var map_scale = 10;
+var map;
+
+// collision testing
+var collision_distance = 8;
+var epsilon = 0.1;
+var ray, dirVec;
+
+// player bounding box size
+var bbox_mins = [-0.5, -2.0, -0.5];
+var bbox_maxs = [0.5, 0.5, 0.5];
+
+// movement
+var pitchObject, yawObject;
+var velocity = new THREE.Vector3();
+var moveForward = false;
+var moveBackward = false;
+var moveLeft = false;
+var moveRight = false;
+var canJump = false;
+
+// networking
+var socket;
+var uid;
+var map_uuid;
+var players = {}
+
+// game logic
+var score = 0;
+var RELOAD_TIME = 1000;
+
+// misc constants
+var PI_2 = Math.PI / 2;
---
+++ weapons.js
@@ -0,0 +1,41 @@
+var throttledShoot = _.throttle(shoot, RELOAD_TIME, {trailing: false}); // refire time >:-(
+
+var shoot = function (){
+	var dirVector = new THREE.Vector3(1,0,0);
+	dirVector = getLookDirection(dirVector);
+	var ray = new THREE.Raycaster();
+	ray.set( yawObject.position, dirVector);
+
+	var intersect_objs = [map];
+	_.each(players, function(player) {
+		intersect_objs.push(player.model);
+	});
+
+	var intersections = ray.intersectObjects(intersect_objs);
+	if(intersections.length) {
+		intersections.sort(function(a, b) {
+			if(a.distance <= b.distance)
+				return -1;
+			return 1;
+		});
+		if(intersections[0].object.uuid === map_uuid) {
+			console.log('hit map, distance: ' + intersections[0].distance);
+		} else {
+			console.log('hit player, distance: ' + intersections[0].distance);
+			//console.log('model uuid: ' + intersections[0].object.uuid);
+			_.each(players, function(player, uid) {
+				// found the player that we hit
+				if (player.model.uuid === intersections[0].object.uuid) {
+					//console.log('hit player uid ' + uid);
+					socket.emit('hit', {
+						'uid': uid
+					});
+
+					updateScore(1);
+					return;
+				}
+			});
+		}
+	}
+}
+
