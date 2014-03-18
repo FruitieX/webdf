c8e40271cfc20de1923589dd7efbc09c532d16b8
Rasmus Eskola <fruitiex@gmail.com>
2014-03-18 23:13:11 +0200

    WIP

--- movement.js
+++ movement.js
@@ -7,11 +7,20 @@ var doMove = function(delta) {
 		modifier = 0.005;
 	}
 
-	if ( moveForward ) velocity.z -= modifier * delta;
-	if ( moveBackward ) velocity.z += modifier * delta;
+	var wishDir = new THREE.Vector3();
 
-	if ( moveLeft ) velocity.x -= modifier * delta;
-	if ( moveRight ) velocity.x += modifier * delta;
+	var dirVec = new THREE.Vector3();
+	dirVec = getLookDirection(new THREE.Vector3());
+	var dirVec_rotated = new THREE.Vector3();
+	dirVec_rotated = getLookDirection(new THREE.Vector3()).applyEuler( new THREE.Euler(0, Math.PI/2, 0, "XYZ" ));
+
+	if ( moveForward ) wishDir.add(dirVec.multiplyScalar(modifier * delta));
+	if ( moveBackward ) wishDir.add(dirVec.multiplyScalar(-modifier * delta));
+
+	if ( moveLeft ) wishDir.add(dirVec_rotated.multiplyScalar(modifier * delta));
+	if ( moveRight ) wishDir.add(dirVec_rotated.multiplyScalar(-modifier * delta));
+
+	velocity.add(wishDir);
 
 	if (onGround === true) {
 		// clip y velocity so we don't fall through
@@ -30,19 +39,16 @@ var doMove = function(delta) {
 		velocity.y -= 0.075 * delta;
 	}
 
-	if (onGround === true && jump) {
-	}
-
-	yawObject.translateX( velocity.x );
-	yawObject.translateY( velocity.y );
-	yawObject.translateZ( velocity.z );
+	yawObject.position.add(velocity);
 
 	onGround = false;
 
+	var velocity_collision_distance = (bbox_mins[0].x / bbox_mins[0].y) * collision_distance;
 	// check direction of velocity vector (+ epsilon)
 	var oldPos = new THREE.Vector3().copy(yawObject.position);
-	ray = new THREE.Raycaster(yawObject.position, velocity, 0, velocity + new THREE.Vector3().copy(velocity).normalize() * epsilon);
+	ray = new THREE.Raycaster(yawObject.position, velocity, 0, new THREE.Vector3().copy(velocity).normalize().multiplyScalar(velocity_collision_distance));
 	var intersections = ray.intersectObjects( [map] );
+	/*
 	if( intersections.length > 0 ) {
 		//console.log('intersection');
 		for(var j = 0; j < intersections.length; j++) {
@@ -50,24 +56,26 @@ var doMove = function(delta) {
 			var distance = intersections[j].distance;
 			var normal = intersections[j].face.normal;
 			//console.log(distance);
-			if ( distance > 0 && distance <= collision_distance) {
+			if ( distance > 0 && distance <= collision_distance + epsilon) {
 				//console.log('intersection: ' + intersections[0].distance);
 
 				// check how large the angle between intersected face normal
 				// and a flat ground plane normal is, set isOnObject accordingly
 				//console.log(normal.dot(new THREE.Vector3(0, 1, 0)));
-				if(normal.dot(new THREE.Vector3(0, 1, 0)) > 0.8) {
-					onGround = true;
-				}
+				//if(normal.dot(new THREE.Vector3(0, 1, 0)) > 0.8) {
+					//onGround = true;
+				//}
 
 				//velocity;
 				// push player back from face along its normal
+				console.log('velocity project');
 				velocity.projectOnPlane(normal);
 				//yawObject.position.add(new THREE.Vector3().copy(normal).multiplyScalar((collision_distance - distance)));
 			}
 		}
 	}
 
+	*/
 	for(var i = 0; i < dirs.length; i++) {
 		ray = new THREE.Raycaster();
 		dirVec = dirs[i];
@@ -97,7 +105,8 @@ var doMove = function(delta) {
 					}
 
 					// push player back from face along its normal
-					//yawObject.position.add(new THREE.Vector3().copy(normal).multiplyScalar((collision_distance - distance)));
+					yawObject.position.add(new THREE.Vector3().copy(normal).multiplyScalar((collision_distance - distance)));
+					velocity.projectOnPlane(normal);
 				}
 			}
 		}
